9c00511309e140e4fa4ea874876078ff77180f26
Musa <puzzlemusa@gmail.com>
2017-07-01 21:41:58 +0600

    Initialized backend project and added git ignore file.

---
+++ Backend/src/main/java/musa/cefalo/assignment/NewsProvider/NewsProvider/NewsProviderApplication.java
@@ -0,0 +1,12 @@
+package musa.cefalo.assignment.NewsProvider.NewsProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsProviderApplication.class, args);
+	}
+}
