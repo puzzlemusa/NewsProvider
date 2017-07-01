dd46f9c660c5c30a8d7d70c85e5914994f9610c3
Musa <puzzlemusa@gmail.com>
2017-07-01 21:59:10 +0600

    Fixed broken app. Updated ComponentScan.

--- Backend/src/main/java/musa/cefalo/assignment/NewsProvider/NewsProvider/config/NewsProviderApplication.java
+++ Backend/src/main/java/musa/cefalo/assignment/NewsProvider/NewsProvider/config/NewsProviderApplication.java
@@ -2,8 +2,10 @@ package musa.cefalo.assignment.NewsProvider.NewsProvider.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"musa.cefalo.assignment.NewsProvider.controller", "musa.cefalo.assignment.NewsProvider.config", "musa.cefalo.assignment.NewsProvider.services"})
 public class NewsProviderApplication {
 
 	public static void main(String[] args) {
